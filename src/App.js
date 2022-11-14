c20cc45c67ef692f9be2c6bb27375737882c7861
CompileWithQureshi <99005823+CompileWithQureshi@users.noreply.github.com>
2022-11-14 05:32:50 -0800

    Save

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css'
-import { Card } from './Card';
+import Card from './Card';
 import Hero from './Hero';
 import Navbar from './Navbar';
+
+import image_12 from './image 12.png'
+import mountain from './mountain-bike 1.png'
+import wedding from './wedding-photography 1.png'
+
 
 function App(params) {
 
@@ -10,7 +15,10 @@ function App(params) {
     <div>
       <Navbar />
       <Hero />
-      <Card img="./image 12.png" no="(6)" contry="USA" qout="Life is hard (test)" price=" $134" />
+      <Card img={image_12} no="(6)" contry="USA" qout="Life is hard (test)" price=" $134" /><br />
+      <Card img={mountain} no="(7)" contry="SA" qout="(test2)" price=" $100" />
+      <Card img={wedding} no="(8)" contry="UA" qout="(test3)" price=" $200" />
+
     </div>
   )
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import "./App.css";
-import image_12 from "./image 12.png";
+// import image_12 from "./image 12.png";
 
-export const Card = () => {
+const Card = (props) => {
+  console.log(props);
   return (
-    <div className="card">
-      <img src={image_12} alt="12-img" className="card-img" />
-      <div className="">
-        <span>*</span>
-        <span className="gray">(6).</span>
-        <span className="gray">USA</span>
+    <div className="container">
+      <div className="card">
+        <img src={props.img} alt="img-type" className="card-img" />
+        <div className="card-stats">
+          <span>*</span>
+          <span className="gray">{props.no}</span>
+          <span className="gray">{props.contry}</span>
+        </div>
+        <p>{props.qout}</p>
+        <p>
+          <span className="bold">{props.price}</span>
+        </p>
       </div>
-      <p>Life lessons from Katie Zaferes</p>
-      <p>
-        <span className="bold">From 130$/person</span>
-      </p>
     </div>
   );
 };
+
+export default Card;
