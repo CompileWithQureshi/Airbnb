630c082357e4e1810106c926e8e1d45860eee321
CompileWithQureshi <99005823+CompileWithQureshi@users.noreply.github.com>
2022-11-15 08:30:27 -0800

    L

--- src/Card.jsx
+++ src/Card.jsx
@@ -5,20 +5,20 @@ import "./App.css";
 const Card = ({ img, no, contry, qout, price }) => {
   console.log({ img, no, contry, qout, price });
   return (
-    // <div className="container">
-    <div className="card">
-      <img src={img} alt="img-type" className="card-img" />
-      <div className="card-stats">
-        <span>*</span>
-        <span className="gray">{no}</span>
-        <span className="gray">{contry}</span>
+    <div className="container">
+      <div className="card">
+        <img src={img} alt="img-type" className="card-img" />
+        <div className="card-stats">
+          <span>*</span>
+          <span className="gray">{no}</span>
+          <span className="gray">{contry}</span>
+        </div>
+        <p>{qout}</p>
+        <p>
+          <span className="bold">{price}</span>
+        </p>
       </div>
-      <p>{qout}</p>
-      <p>
-        <span className="bold">{price}</span>
-      </p>
     </div>
-    // </div>
   );
 };
 
