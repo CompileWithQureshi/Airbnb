fd5e59c764c4d70d8d5c2491238ced46d884dbaf
CompileWithQureshi <99005823+CompileWithQureshi@users.noreply.github.com>
2022-11-17 09:39:04 -0800

    Rewriting card props

--- src/App.js
+++ src/App.js
@@ -3,21 +3,26 @@ import './App.css'
 import Card from './Card';
 import Hero from './Hero';
 import Navbar from './Navbar';
+import data from './data'
 
 // import image_12 from './image 12.png'
 
-
+console.log(data);
 
 function App(params) {
+  const cards = data.map(item => {
+    return (
+      <Card img={item.coverImg} rating={item.stats.rating} reviewCount={item.stats.reviewCount} location={item.location} qout={item.title} price={item.price} />
+
+    )
+  })
+  console.log(cards);
 
   return (
     <div>
       <Navbar />
       <Hero />
-      <div className='holder'>
-
-        <Card img='../public/images/image 12.png' no={6} contry="USA" qout="Life is hard (test)" price={130} /><br />
-      </div>
+      {cards}
     </div>
   )
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 // import image_12 from "./image 12.png";
 
 const Card = (props) => {
-  const { img, no, contry, qout, price } = props;
+  const { img, reviewCount, location, qout, price } = props;
   console.log(props);
   return (
     <div className="container">
@@ -11,8 +11,8 @@ const Card = (props) => {
         <img src={img} alt="img-type" className="card-img" />
         <div className="card-stats">
           <span>*</span>
-          <span className="gray">{no}</span>
-          <span className="gray">{contry}</span>
+          <span className="gray">{reviewCount}</span>
+          <span className="gray">{location}</span>
         </div>
         <p>{qout}</p>
         <p>
